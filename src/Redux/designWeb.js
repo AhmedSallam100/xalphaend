311cc075d9c44c6f1282d4d5ece75ad8c3886a06
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-08-26 19:45:38 +0300

    Update designWeb.js

--- src/Redux/designWeb.js
+++ src/Redux/designWeb.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const designWebApi = createApi({
   reducerPath: "designWebApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_BASE_URL}/api/`,
+    baseUrl: `https://xalphaend.onrender.com/api/`,
   }),
   endpoints: (builder) => ({
     getdesignWebByName: builder.query({
