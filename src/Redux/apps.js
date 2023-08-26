1b96e95c6aa96280a05790581ce132c5147fee1e
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-08-26 19:44:50 +0300

    Update apps.js

--- src/Redux/apps.js
+++ src/Redux/apps.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const appApi = createApi({
   reducerPath: "appApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_BASE_URL}/api/`,
+    baseUrl: `https://xalphaend.onrender.com/api/`,
   }),
   endpoints: (builder) => ({
     getappByName: builder.query({
