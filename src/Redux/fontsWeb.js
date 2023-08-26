910b6fabe56f28c9c51e9f5a7a5eb992aefddec5
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-08-26 19:46:01 +0300

    Update fontsWeb.js

--- src/Redux/fontsWeb.js
+++ src/Redux/fontsWeb.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const fontsWebApi = createApi({
   reducerPath: "fontsWebApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_BASE_URL}/api/`,
+    baseUrl: `https://xalphaend.onrender.com/api/`,
   }),
   endpoints: (builder) => ({
     getfontsWebByName: builder.query({
