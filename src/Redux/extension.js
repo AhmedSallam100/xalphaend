618f36f9a056e831dee75961e59871ddbcd189e9
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-08-26 19:45:48 +0300

    Update extension.js

--- src/Redux/extension.js
+++ src/Redux/extension.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const extensionApi = createApi({
   reducerPath: "extensionApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_BASE_URL}/api/`,
+    baseUrl: `https://xalphaend.onrender.com/api/`,
   }),
   endpoints: (builder) => ({
     getextensionByName: builder.query({
