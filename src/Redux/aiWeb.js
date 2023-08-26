b3167c5b2a758c4408e3787d6d1ac1102230107e
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-08-26 19:43:44 +0300

    Update aiWeb.js

--- src/Redux/aiWeb.js
+++ src/Redux/aiWeb.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const aiWebApi = createApi({
   reducerPath: "aiWebApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://xalphaend.vercel.app/api/`,
+    baseUrl: `https://xalphaend.onrender.com/api/`,
   }),
   endpoints: (builder) => ({
     getaiWebByName: builder.query({
