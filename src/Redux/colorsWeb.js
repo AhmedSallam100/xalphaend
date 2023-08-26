21dd0e0b76abe6326ab80ea993adc50de6b08ee9
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-08-26 19:45:21 +0300

    Update colorsWeb.js

--- src/Redux/colorsWeb.js
+++ src/Redux/colorsWeb.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const colorsWebApi = createApi({
   reducerPath: "colorsWebApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_BASE_URL}/api/`,
+    baseUrl: `https://xalphaend.onrender.com/api/`,
   }),
   endpoints: (builder) => ({
     getcolorsWebByName: builder.query({
